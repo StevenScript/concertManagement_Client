77c1fae603d08fd6e74f8822b477e9814868979a
StevenScript <sdn709466@gmail.com>
2025-02-28 01:20:11 -0330

    ADD - Lombok Dependency/ Updated Artist class to match server

--- src/main/java/com/concertManager/model/Artist.java
+++ src/main/java/com/concertManager/model/Artist.java
@@ -1,41 +1,16 @@
 package com.concertManager.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Artist {
     private Long id;
     private String stageName;
     private String genre;
     private Integer membersCount;
     private String homeCity;
-
-    // Getters/Setters
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getStageName() {
-        return stageName;
-    }
-    public void setStageName(String stageName) {
-        this.stageName = stageName;
-    }
-    public String getGenre() {
-        return genre;
-    }
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-    public Integer getMembersCount() {
-        return membersCount;
-    }
-    public void setMembersCount(Integer membersCount) {
-        this.membersCount = membersCount;
-    }
-    public String getHomeCity() {
-        return homeCity;
-    }
-    public void setHomeCity(String homeCity) {
-        this.homeCity = homeCity;
-    }
 }
