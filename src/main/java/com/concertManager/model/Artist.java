b395bcdb29b83a6020c3c21ed4d4984b43741895
StevenScript <sdn709466@gmail.com>
2025-02-28 01:29:15 -0330

    ADD - Event context for Artist

--- src/main/java/com/concertManager/model/Artist.java
+++ src/main/java/com/concertManager/model/Artist.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,4 +16,5 @@ public class Artist {
     private String genre;
     private Integer membersCount;
     private String homeCity;
+    private Set<Event> events = new HashSet<>();
 }
