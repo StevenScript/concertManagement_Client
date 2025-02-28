5c39d18fbb016812dc09c807ace891bac017989d
StevenScript <sdn709466@gmail.com>
2025-02-28 01:25:40 -0330

    ADD - Event DTO Class

---
+++ src/main/java/com/concertManager/model/Event.java
@@ -0,0 +1,20 @@
+package com.concertManager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    private Long id;
+    private LocalDate eventDate;
+    private Double ticketPrice;
+    private Integer availableTickets;
+    private Set<Artist> artists = new HashSet<>();
+}
